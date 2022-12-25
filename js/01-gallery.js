d208d9cf072a1062c8cff806737b95ddc02124a4
Uliana Shymanska <yliana.shymanska1999@gmail.com>
2022-12-25 18:51:58 +0200

    done js-hw-7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,46 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallerymarkUp = galleryItems
+  .map(
+    item =>
+      `<div class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+       data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`
+  )
+  .join('');
+
+const gallery = document.querySelector('.gallery');
+
+gallery.insertAdjacentHTML('beforeend', gallerymarkUp);
+gallery.addEventListener('click', oneImgContenerClick);
+
+function oneImgContenerClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const selectedImage = event.target.dataset.source;
+  /* const selectedImage = event.target.getAttribute("data-source"); */
+
+  const instance = basicLightbox.create(`
+    <img src="${selectedImage}" width="800" height="600">
+`);
+
+  instance.show();
+
+  gallery.addEventListener('keydown', event => {
+    if (event.key === 'Escape') {
+      instance.close();
+    }
+  });
+}
